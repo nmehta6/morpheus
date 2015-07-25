4d2dd14e0f90bb1c4397e6f25e5cb9b8184720c9
nachiket mehta <nmehta@mediafly.com>
2015-07-25 11:57:46 -0500

    fix mocha setup

--- src/morpheus.js
+++ src/morpheus.js
@@ -1,10 +1,25 @@
-let validate = require('jsonschema').validate
+let validate  = require('jsonschema').validate
+let {compose, keys} = require('ramda')
+
 class Morpheus {
 	constructor() {
 		this.check = validate
 	}
-	map(fromSchema, to) {
-		return to;
+	map(fromSchema, toSchema, fromObj) {
+
+
+		let toObj = {}
+		for (var prop in toSchema.properties) {
+			if (fromObj.hasOwnProperty(prop)) {
+				toObj[prop] = fromObj[prop]
+			}
+		}
+
+		return toObj
+
+
+
+
 	}
 }
 
--- test/morpheus.spec.js
+++ test/morpheus.spec.js
@@ -1,8 +1,8 @@
 let expect = require('chai').expect
+let morpheus = require('../index')
 
-describe('morpheus should', (argument) => {
-	describe('check schema', () => {
-		let morpheus = require('../index')
+describe('morpheus', (argument) => {
+	it('should validate schema', () => {
 		let schema = {
 			type: 'number'
 		}
@@ -11,7 +11,30 @@ describe('morpheus should', (argument) => {
 		let actual = morpheus.check(instance, schema)
 		expect(actual.errors).to.have.length(0)
 	})
+	it('should map schema', () => {
+		let fromSchema = {
+			type: 'object',
+			properties: {
+				name: { type: 'string' },
+				address: { type: 'string' },
+				zip: { type: 'number' }
+			}
+		}
+		let toSchema = {
+			type: 'object',
+			properties: {
+				name: { type: 'string' },
+				zip: { type: 'string' }
+			}
+		}
+		let fromObj = {
+			name: 'Nach',
+			address: 'Palatine, IL',
+			zip: 60074
+		}
 
-
-
+		let actual = morpheus.map(fromSchema, toSchema, fromObj)
+		expect(actual)
+			.to.have.property('name').equal('Nach')
+	})
 })
