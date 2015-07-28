66766093a2cf8b591148273c40d962a5d1bbc220
nachiket mehta <nmehta@mediafly.com>
2015-07-27 23:57:51 -0500

    validate to and from schemas

--- src/morpheus.js
+++ src/morpheus.js
@@ -3,7 +3,7 @@ var {compose, defaultTo, find, map, mergeAll, prop, propEq, toPairs} = require('
 
 class Morpheus {
 	constructor() {
-		this.validate = validate
+		this.validate = (instance, schema) => validate(instance, schema, {throwError: true})
 		this.registrations = []
 	}
 	register(registration) {
@@ -11,7 +11,10 @@ class Morpheus {
 		return this.registrations
 	}
 	map(id, fromObj) {
-		var getSchemaProps = compose(toPairs, prop('properties'), prop('toSchema'), find(propEq('id', id)))
+		var getRegistration = find(propEq('id', id))
+		var getFromSchema = compose(prop('fromSchema'), getRegistration)
+		var getToSchema = compose(prop('toSchema'), getRegistration)
+		var getSchemaProps = compose(toPairs, prop('properties'), getToSchema)
 		var applyHandler = ([key, schema]) => {
 			var value = defaultTo(prop(key))(schema.handler)
 			return { [key]: value(fromObj)}
@@ -19,7 +22,15 @@ class Morpheus {
 		var mapProps = compose(mergeAll, map(applyHandler))
 
 		var mapObj = compose(mapProps, getSchemaProps)
+
 		var result = mapObj(this.registrations)
+
+		//validate from and to obj
+		this.validate(fromObj, getFromSchema(this.registrations))
+		this.validate(result, getToSchema(this.registrations))
+		// var fromValidation = this.validate(result, )
+
+
 
 		return result
 	}
